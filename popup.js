fadabec93dab53ce412e064ea3fd6ca95f95ca0a
Alexei <174282@gmail.com>
2017-08-27 03:05:29 +0300

    init
    - first draft

---
+++ popup.js
@@ -0,0 +1,63 @@
+﻿function convert() {
+    chrome.tabs.executeScript({
+        file: 'script.js'
+    });
+}
+
+document.getElementById('convert').addEventListener('click', convert);
+
+
+
+
+// Saves options to chrome.storage
+function save_options() {
+    var nok = document.getElementById('nok').value;
+    var ron = document.getElementById('ron').value;
+
+    chrome.storage.sync.set({
+        nok: nok,
+        ron: ron
+    }, function() {
+        // Update status to let user know options were saved.
+        var status = document.getElementById('status');
+        status.textContent = 'Options saved.';
+        setTimeout(function() {
+            status.textContent = '';
+        }, 750);
+    });
+}
+
+// stored in chrome.storage.
+function restore_options() {
+
+    chrome.storage.sync.get({
+        nok: 9.2,
+        ron: 4.5
+    }, function(items) {
+        document.getElementById('nok').value = items.nok;
+        document.getElementById('ron').value = items.ron;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restore_options);
+document.getElementById('save').addEventListener('click', save_options);
+document.getElementById('getCurrency').addEventListener('click', getCurency);
+
+function getCurency() {
+    $.get( "https://api.fixer.io/latest?base=EUR", function(data){
+        chrome.storage.sync.set({
+            nok: data['rates']['NOK'],
+            ron: data['rates']['RON'],
+        }, function() {
+            // Update status to let user know options were saved.
+            var status = document.getElementById('status');
+            status.textContent = 'Loading ...';
+            setTimeout(function() {
+                document.getElementById('nok').value = data['rates']['NOK'];
+                document.getElementById('ron').value = data['rates']['RON'];
+                status.textContent = '';
+            }, 750);
+        });
+    });
+}
+
---
+++ script.js
@@ -0,0 +1,58 @@
+﻿    window.setTimeout(function(){
+	    var exchangeValues = getExchangeRate(currencies);
+
+        replaceLocalCurrency(exchangeValues['rate'],exchangeValues['abr']);
+	}, 0);
+	
+	function replaceLocalCurrency(rate, abr){
+        var flightsDivs = document.getElementsByClassName( 'rf-fare__price__current' );
+        var calendarDivs = document.getElementsByClassName( 'booking-flow__flight-select__chart__day__price' );
+
+        replaceOneCurrency(flightsDivs, rate, abr);
+        replaceOneCurrency(calendarDivs, rate, abr);
+	}
+
+	function replaceOneCurrency(divs, rate, abr) {
+        [].slice.call( divs ).forEach(function ( div ) {
+            var currency = div.innerHTML.match(new RegExp(abr, 'i'));
+
+            if(currency && currency[0].length>0) {
+                div.innerHTML = div.innerHTML.replace(abr, '').replace(',', '').replace(' ', '');
+                var amount = div.innerHTML.match(/[\d]{1,9}/g);
+                div.innerHTML = "€" + parseInt(amount[0] / rate)
+            }
+        });
+    }
+
+	function getExchangeRate(currencies) {
+        var divs = document.getElementsByClassName( 'booking-flow__flight-select__chart__day__price' );
+        var price = divs[0].innerHTML;
+        var currency = [];
+        var exchangeCurrency = [];
+
+        $.each(currencies, function (abr , value) {
+            currency = price.match(new RegExp(value[1], 'i'));
+            if(currency){
+                exchangeCurrency['rate'] = value[2];
+                exchangeCurrency['abr'] = value[1];
+            }
+        });
+
+        return exchangeCurrency;
+    }
+
+    var currencies = [
+        ['NOK','kr', 9.2],
+        ['RON','lei', 4.5]
+    ]
+
+    chrome.storage.sync.get({
+        nok: 9.2,
+        ron: 4.5
+    }, function(items) {
+        window.currencies = [
+            ['NOK','kr', items.nok],
+            ['RON','lei', items.ron]
+        ]
+
+    });
